090bc895bc10e54f29213f446ba64d21d3c5f1c3
Shaurya0987 <shaurya3537@gmail.com>
2025-12-06 12:40:11 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BiteBuddyY',
+      title: 'BiteBuddy',
       home: SplashScreen()
     );
   }
